a2ad5dd4ce5c6c4ec531623bffcbc85c1c1c6ce5
north-o <northweel@yahoo.com>
2019-01-24 12:12:25 -0800

    check in time! :)

--- winningpercentage.py
+++ winningpercentage.py
@@ -1,16 +1,35 @@
 # A game with stats
 import random
 
-Game = random.randint(1, 100)
-print (Game)
-
 Wins = 0
-Loses = 0
+Losses = 0
+counter = 0
+maxrange = 150
 
-if Game > 50:
-    print("You won! :) ")
-    Wins = Wins+1
-else:
-    print("Better luck next time... ):")
-    Loses = Loses+1
 
+
+for counter in range(maxrange):
+
+    Game = random.randint(1, 100)
+    print (Game)
+
+    if Game > 50:
+        print("You won! :) ")
+        Wins = Wins+1
+    else:
+        print("Better luck next time... ):")
+        Losses = Losses+1
+
+    Score = Wins-Losses
+    Gamesplayed = Wins+Losses
+    WinningPercentage = Wins/Gamesplayed
+
+    print ("You have", Wins, "Wins")
+    print ("You have", Losses, "Loses")
+    print ("Your score is", Score)
+    print ("You have played", Gamesplayed, "Games.")
+    print ("WinningPercentage is", WinningPercentage)
+
+    Again = input("Do you want to play again? Y/N")
+    if Again == "N":
+        break
